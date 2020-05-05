886f9b950704ed6727dda4c162677ae397b482ce
Zaphie <ddnmikbit@gmail.com>
2020-05-05 11:28:11 -0400

    Load the new secrets of the users

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema ({
   email: String,
   password: String,
-  googleId:String
+  googleId:String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -100,11 +101,42 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+  User.find({"secret": {$ne:null}}, function(er, foundUsers){
+    if (err){
+      cocnsole.log(err);
+    } else{
+      if (foundUsers){
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+
+});
+
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }else{
     res.redirect("/login");
   }
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+  //buscar el usuario actual y guardar su secreto en sus datos
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if (foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
 
 });
 
